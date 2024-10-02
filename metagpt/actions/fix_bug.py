83b01894002794fc77db53ee30e219aa9623172c
huhtig <tizoeypinoy@gmail.com>
2024-10-02 16:49:04 +0800

    Update fix_bug.py
    UPDATE COMMIT CHANGES

--- metagpt/actions/fix_bug.py
+++ metagpt/actions/fix_bug.py
@@ -1,13 +1,1 @@
-# -*- coding: utf-8 -*-
-"""
-@Time    : 2023-12-12
-@Author  : mashenquan
-@File    : fix_bug.py
-"""
-from metagpt.actions import Action
-
-
-class FixBug(Action):
-    """Fix bug action without any implementation details"""
-
-    name: str = "FixBug"
+FIX
