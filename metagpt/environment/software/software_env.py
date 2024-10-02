6c49e6bc881a4f39a98f0d6043daadc6b88e2b01
huhtig <tizoeypinoy@gmail.com>
2024-10-02 16:52:17 +0800

    Update software_env.py
    UPDATE COMMIT CHANGES

--- metagpt/environment/software/software_env.py
+++ metagpt/environment/software/software_env.py
@@ -1,12 +1,1 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Desc   : MG Software Env
-
-
-from metagpt.environment.base_env import Environment
-
-
-class SoftwareEnv(Environment):
-    """a specific alias name"""
-
-    pass
+FIX
